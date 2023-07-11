d9b9043d29ef930156d9e8edc3ebebd42e89873c
Rob Pavlik <RxPx39@gmail.com>
2023-07-11 02:36:23 -0400

    Part B2 -
    Added all needed getters and setters to Vacation Class to allow implementation of Vacation title, lodging, start date, and end date

--- app/src/main/java/com/myappcompany/rob/d308_mobileapp/entities/Vacation.java
+++ app/src/main/java/com/myappcompany/rob/d308_mobileapp/entities/Vacation.java
@@ -2,9 +2,12 @@ package com.myappcompany.rob.d308_mobileapp.entities;
 
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 @Entity(tableName = "vacations")
 @TypeConverters(Vacation.DateConverter.class)
@@ -27,3 +30,76 @@ public class Vacation {
 
     public Vacation() {
     }
+
+    public String getVacationLodging() {
+        return vacationLodging;
+    }
+
+    public void setVacationLodging(String vacationLodging) {
+        this.vacationLodging = vacationLodging;
+    }
+
+    public int getVacationID() {
+        return vacationID;
+    }
+
+    public void setVacationID(int vacationID) {
+        this.vacationID = vacationID;
+    }
+
+    public String getVacationName() {
+        return vacationName;
+    }
+
+    public void setVacationName(String vacationName) {
+        this.vacationName = vacationName;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Vacation{" +
+                "vacationID=" + vacationID +
+                ", vacationName='" + vacationName + '\'' +
+                ", vacationLodging='" + vacationLodging + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+
+    public static class DateConverter {
+
+        @TypeConverter
+        public Date toDate(Long timestamp) {
+            return timestamp == null ? null : new Date(timestamp);
+        }
+
+        @TypeConverter
+        public Long toTimestamp(Date date) {
+            return date == null ? null : date.getTime();
+        }
+
+        @TypeConverter
+        public static String fromDate(Date date) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+            return date == null ? null : dateFormat.format(date);
+        }
+    }
+
+
+}
